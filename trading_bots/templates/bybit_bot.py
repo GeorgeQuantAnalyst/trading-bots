339bd487d4020852a3bbaadf4e70bf13c6de059f
GeorgeQuantAnalyst <jiri.caga@proton.me>
2023-05-17 13:26:47 +0200

    BybitBot: fixed constructor

--- trading_bots/templates/bybit_bot.py
+++ trading_bots/templates/bybit_bot.py
@@ -5,7 +5,7 @@ from pybit.unified_trading import HTTP
 class BybitBot(Bot):
 
     def __init__(self, config: dict):
+        super().__init__(config)
         self.exchange = HTTP(testnet=config["bybitApi"]["testnet"],
                              api_key=config["bybitApi"]["apiKey"],
                              api_secret=config["bybitApi"]["secretKey"])
-        super().__init__(config)
