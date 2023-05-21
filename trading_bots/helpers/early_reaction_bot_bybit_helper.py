4bcfa60ff7bd306bf0c522a8a72d244c13923297
GeorgeQuantAnalyst <jiri.caga@proton.me>
2023-05-21 15:08:17 +0200

    EarlyReactionBotBybit: implemented get_last_bar function

--- trading_bots/helpers/early_reaction_bot_bybit_helper.py
+++ trading_bots/helpers/early_reaction_bot_bybit_helper.py
@@ -1,5 +1,3 @@
-import logging
-
 import pandas as pd
 
 from trading_bots import constants
@@ -12,19 +10,35 @@ class EarlyReactionBotBybitHelper:
         self.instruments_info = pybit_client.get_instruments_info(category=constants.BYBIT_LINEAR_CATEGORY)["result"][
             "list"]
 
-    def get_open_positions(self):
+    def get_open_positions(self) -> list:
         response = self.pybit_client.get_positions(category=constants.BYBIT_LINEAR_CATEGORY, settleCoin="USDT")
         positions = pd.DataFrame(response["result"]["list"])
         return positions
 
-    def check_if_position_has_tp_and_sl(self):
+    def check_if_position_has_tp_and_sl(self) -> bool:
         pass
 
     def get_actual_price(self):
         pass
 
-    def get_ohlc(self):
-        pass
+    def get_last_bar(self, ticker: str, interval: int = 1) -> dict:
+        response = self.pybit_client.get_kline(
+            symbol=ticker,
+            category=constants.BYBIT_LINEAR_CATEGORY,
+            interval=interval,
+            limit=1
+        )
+
+        last_bar = response["result"]["list"][0]
+        return {
+            "startTime": last_bar[0],
+            "openPrice": last_bar[1],
+            "highPrice": last_bar[2],
+            "lowPrice": last_bar[3],
+            "closePrice": last_bar[4],
+            "volume": last_bar[5],
+            "turnover": last_bar[6]
+        }
 
     def get_before_entry_ids(self):
         pass
@@ -37,6 +51,3 @@ class EarlyReactionBotBybitHelper:
 
     def remove_not_existing_ids(self):
         pass
-
-
-
