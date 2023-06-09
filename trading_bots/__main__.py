fb78cefa3e596448795fc4f1fc15bc7a45b85bb0
GeorgeQuantAnalyst <jiri.caga@proton.me>
2023-06-09 10:40:24 +0200

    fixed exception handling - added log message for SystemExit exception

--- trading_bots/__main__.py
+++ trading_bots/__main__.py
@@ -29,7 +29,8 @@ if __name__ == "__main__":
         config = load_config(config_file_path)
         bot = create_bot(bot_name, config)
         bot.run()
-    except SystemExit:
+    except SystemExit as e:
+        logging.warning("Close application with code: {}".format(str(e)))
         pass
     except Exception as e:
         logging.exception("Error in app: ")
