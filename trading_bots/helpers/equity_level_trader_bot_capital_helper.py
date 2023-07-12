4dadf58ec7adf20daf8df45b982a2d135dcf8c88
lucka <lucka.work@outlook.com>
2023-07-12 15:34:49 +0200

    equity_level_trader_bot_capital_helper: preparing logic and tests

--- trading_bots/helpers/equity_level_trader_bot_capital_helper.py
+++ trading_bots/helpers/equity_level_trader_bot_capital_helper.py
@@ -1,12 +1,16 @@
 import csv
 import datetime
+import logging
 from datetime import time
+
+import requests
 
 
 class EquityLevelTraderBotCapitalHelper:
 
     def __init__(self, config: dict):
         self.capital_client = None
+        self.api_key = config["alphavantageApiKey"]["apiKey"]
 
     @staticmethod
     def load_orders(file_path):
@@ -36,10 +40,22 @@ class EquityLevelTraderBotCapitalHelper:
 
         return False
     @staticmethod
-    def was_earnings_yesterday(ticker) -> bool:
+    def was_yesterday_the_last_earnings(self, ticker) -> bool:
         now = datetime.datetime.now().date()
-        # TODO: Lucka
-        return False
+        yesterday = now - datetime.timedelta(days=1)
+
+        url = f"https://www.alphavantage.co/query?function=EARNINGS&symbol={ticker}&apikey={self.api_key}"
+        response = requests.get(url)
+        data = response.json()
+
+        earnings_types = ["quarterlyEarnings", "annualEarnings"]
+        for earnings_type in earnings_types:
+            earnings = data.get(earnings_type, [])
+            if earnings and datetime.datetime.strptime(earnings[0]["reportedDate"], "%Y-%m-%d").date() == yesterday:
+                logging.debug(f"The last earnings result ({earnings_type}) was yesterday: {yesterday}")
+                return True
+            else:
+                return False
 
     @staticmethod
     def is_earnings_next_days(ticker: str, count_days: int = 10) -> bool:
